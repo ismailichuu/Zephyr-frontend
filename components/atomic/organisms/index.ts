4f5648dd2f5c0a8d388ec770ece3647608819443
Ismail <ismailichu2001@gmail.com>
2026-03-07 16:09:31 +0530

    refactor: edit in the proxy middlware corresponding to refresh token. created two seperate instances for client and server

--- components/atomic/organisms/index.ts
+++ components/atomic/organisms/index.ts
@@ -4,7 +4,6 @@ export { default as ForgotPassword } from "./ForgotPassword";
 export { default as RoleSelector } from "./RoleSelector";
 export { default as EmailVerification } from "./EmailVerification";
 export { default as ResetPassword } from "./ResetPassword";
-export { default as FreelancerCompleteProfile } from "./FreelancerCompleteProfile";
 export { default as AdminUserManagement } from "./AdminUserManagement";
 export { default as AdminUserDetails } from "./AdminUserDetails";
 export type { AdminUserDetailsData } from "./AdminUserDetails";
